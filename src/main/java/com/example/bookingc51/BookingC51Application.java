055dcb872deba723ca9ff8fb1dec2c9cef177788
ElizaTereshko <elizaveta.tereshko98@gmail.com>
2022-05-07 01:51:13 +0300

    first commit

---
+++ src/main/java/com/example/bookingc51/BookingC51Application.java
@@ -0,0 +1,13 @@
+package com.example.bookingc51;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingC51Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingC51Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bookingc51/BookingC51ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bookingc51;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingC51ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
